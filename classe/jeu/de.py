625aa41d923bae35173dbfe1a04c58da0e0d6a95
Leeexyy4 <119850828+Leeexyy4@users.noreply.github.com>
2024-06-05 13:46:22 +0200

    Ajout de l'image 0, lancement de auto par ia

--- classe/jeu/de.py
+++ classe/jeu/de.py
@@ -3,6 +3,7 @@
 # Bibliotheques utilisees pour le code
 import pygame
 from random import randint
+from classe.personnage import intelA
 
 #pour les images : De creees par Moi-meme side Piskel
 
@@ -17,114 +18,122 @@ class De:
         """Getter de la face aleatoire choisie."""
         return self.__face_choisie
 
-    def set_face_choisie(self, x):
+    def set_face_choisie(self, face_choisie):
+        """Setter de la face aleatoire choisie."""
+        self.__face_choisie = face_choisie
+
+    def desincrement_face_choisie(self, x):
         """Setter de la face aleatoire choisie."""
         self.__face_choisie = self.__face_choisie - x
 
-    def Choix_de(self, surface):
+    def Choix_de(self, interface, joueur):
         """La fonction Choix_De retourne la face aleatoire choisie du de (Surface surface).
 
         Returns:
             int face_choisie: Retourne la face aleatoire choisie
         """
-        de_cliquer = "False"
-        while de_cliquer != "True":
-            mouse_x, mouse_y = pygame.mouse.get_pos() # Recuperer les coordonnees de la souris
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT: # si le joueur quitte la fenetre # si le joueur quitte la fenetre
-                    pygame.quit()
-                    exit()   
-                # Si le personnage sur lequel on clique est Kevin
-                if 350 <= mouse_x <= 435 and 475 <= mouse_y <= 560 :   
-                    if event.type == pygame.MOUSEBUTTONDOWN : # Si le joueur clique sur le bouton, on passe à la prochaine page "introduction"
-                        
-                        # Affiche le de sur la face 2
-                        de_face2 = pygame.image.load("./assets/img/de/Face2.png")
-                        surface.blit(de_face2,(350,475))
-                        # Mettre à jour l'affichage
-                        pygame.display.update()
-                        pygame.time.delay(150)
-                        
-                        # Affiche le de sur la face 3
-                        de_face3 = pygame.image.load("./assets/img/de/Face3.png")
-                        surface.blit(de_face3,(350,475))
-                        # Mettre à jour l'affichage
-                        pygame.display.update()
-                        pygame.time.delay(200)
-                        
-                        # Affiche le de sur la face 4
-                        de_face4 = pygame.image.load("./assets/img/de/Face4.png")
-                        surface.blit(de_face4,(350,475))
-                        # Mettre à jour l'affichage
-                        pygame.display.update()
-                        pygame.time.delay(250)
-                        
-                        # Affiche le de sur la face 5
-                        de_face5 = pygame.image.load("./assets/img/de/Face5.png")
-                        surface.blit(de_face5,(350,475))
-                        # Mettre à jour l'affichage
-                        pygame.display.update()
-                        pygame.time.delay(300)
-                        
-                        # Affiche le de sur la face 1
-                        de_face1 = pygame.image.load("./assets/img/de/Face1.png")
-                        surface.blit(de_face1,(350,475))
-                        # Mettre à jour l'affichage
-                        pygame.display.update()
-                        pygame.time.delay(400)
-                        
-                        # Affiche le de sur la face 6
-                        de_face6 = pygame.image.load("./assets/img/de/Face6.png")
-                        surface.blit(de_face6,(350,475))
-                        # Mettre à jour l'affichage
-                        pygame.display.update()
-                        pygame.time.delay(450)
-                        de_cliquer = "True" 
-                        
-        # Choisir la face aleatoire
-        self.__face_choisie = randint(1,6)
+        if isinstance(joueur,intelA.IntelA):
+            joueur.lancement_de(interface)
+        else:
+            de_cliquer = False
+            while de_cliquer == False:
+                mouse_x, mouse_y = pygame.mouse.get_pos() # Recuperer les coordonnees de la souris
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT: # si le joueur quitte la fenetre # si le joueur quitte la fenetre
+                        pygame.quit()
+                        exit()   
+                    # Si le personnage sur lequel on clique est Kevin
+                    if 350 <= mouse_x <= 435 and 475 <= mouse_y <= 560 :   
+                        if event.type == pygame.MOUSEBUTTONDOWN : # Si le joueur clique sur le bouton, on passe à la prochaine page "introduction"
+                            
+                            # Affiche le de sur la face 2
+                            de_face2 = pygame.image.load("./assets/img/de/Face2.png")
+                            interface.get_fenetre().blit(de_face2,(350,475))
+                            # Mettre à jour l'affichage
+                            pygame.display.update()
+                            pygame.time.delay(150)
+                            
+                            # Affiche le de sur la face 3
+                            de_face3 = pygame.image.load("./assets/img/de/Face3.png")
+                            interface.get_fenetre().blit(de_face3,(350,475))
+                            # Mettre à jour l'affichage
+                            pygame.display.update()
+                            pygame.time.delay(200)
+                            
+                            # Affiche le de sur la face 4
+                            de_face4 = pygame.image.load("./assets/img/de/Face4.png")
+                            interface.get_fenetre().blit(de_face4,(350,475))
+                            # Mettre à jour l'affichage
+                            pygame.display.update()
+                            pygame.time.delay(250)
+                            
+                            # Affiche le de sur la face 5
+                            de_face5 = pygame.image.load("./assets/img/de/Face5.png")
+                            interface.get_fenetre().blit(de_face5,(350,475))
+                            # Mettre à jour l'affichage
+                            pygame.display.update()
+                            pygame.time.delay(300)
+                            
+                            # Affiche le de sur la face 1
+                            de_face1 = pygame.image.load("./assets/img/de/Face1.png")
+                            interface.get_fenetre().blit(de_face1,(350,475))
+                            # Mettre à jour l'affichage
+                            pygame.display.update()
+                            pygame.time.delay(400)
+                            
+                            # Affiche le de sur la face 6
+                            de_face6 = pygame.image.load("./assets/img/de/Face6.png")
+                            interface.get_fenetre().blit(de_face6,(350,475))
+                            # Mettre à jour l'affichage
+                            pygame.display.update()
+                            pygame.time.delay(450)
+                            de_cliquer = True
+                            
+            # Choisir la face aleatoire
+            self.set_face_choisie(randint(1,6))
+            
+            if self.get_face_choisie() == 1:
+                # Affiche le de sur la face 1
+                de_face1 = pygame.image.load("./assets/img/de/Face1.png")
+                interface.get_fenetre().blit(de_face1,(350,475))
+                # Mettre à jour l'affichage
+                pygame.display.update()
+                
+            elif self.get_face_choisie() == 2:
+                # Affiche le de sur la face 2
+                de_face2 = pygame.image.load("./assets/img/de/Face2.png")
+                interface.get_fenetre().blit(de_face2,(350,475))
+                # Mettre à jour l'affichage
+                pygame.display.update()
+                
+            elif self.get_face_choisie() == 3:
+                # Affiche le de sur la face 3
+                de_face3 = pygame.image.load("./assets/img/de/Face3.png")
+                interface.get_fenetre().blit(de_face3,(350,475))
+                # Mettre à jour l'affichage
+                pygame.display.update()
+                
+            elif self.get_face_choisie() == 4:
+                # Affiche le de sur la face 4
+                de_face4 = pygame.image.load("./assets/img/de/Face4.png")
+                interface.get_fenetre().blit(de_face4,(350,475))
+                # Mettre à jour l'affichage
+                pygame.display.update()
+                
+            elif self.get_face_choisie() == 5:
+                # Affiche le de sur la face 5
+                de_face5 = pygame.image.load("./assets/img/de/Face5.png")
+                interface.get_fenetre().blit(de_face5,(350,475))
+                # Mettre à jour l'affichage
+                pygame.display.update()
+                
+            elif self.get_face_choisie() == 6:
+                # Affiche le de sur la face 6
+                de_face6 = pygame.image.load("./assets/img/de/Face6.png")
+                interface.get_fenetre().blit(de_face6,(350,475))
+                # Mettre à jour l'affichage
+                pygame.display.update()
+                
+            return self.get_face_choisie()
         
-        if self.__face_choisie == 1:
-            # Affiche le de sur la face 1
-            de_face1 = pygame.image.load("./assets/img/de/Face1.png")
-            surface.blit(de_face1,(350,475))
-            # Mettre à jour l'affichage
-            pygame.display.update()
-            
-        elif self.__face_choisie == 2:
-            # Affiche le de sur la face 2
-            de_face2 = pygame.image.load("./assets/img/de/Face2.png")
-            surface.blit(de_face2,(350,475))
-            # Mettre à jour l'affichage
-            pygame.display.update()
-            
-        elif self.__face_choisie == 3:
-            # Affiche le de sur la face 3
-            de_face3 = pygame.image.load("./assets/img/de/Face3.png")
-            surface.blit(de_face3,(350,475))
-            # Mettre à jour l'affichage
-            pygame.display.update()
-            
-        elif self.__face_choisie == 4:
-            # Affiche le de sur la face 4
-            de_face4 = pygame.image.load("./assets/img/de/Face4.png")
-            surface.blit(de_face4,(350,475))
-            # Mettre à jour l'affichage
-            pygame.display.update()
-            
-        elif self.__face_choisie == 5:
-            # Affiche le de sur la face 5
-            de_face5 = pygame.image.load("./assets/img/de/Face5.png")
-            surface.blit(de_face5,(350,475))
-            # Mettre à jour l'affichage
-            pygame.display.update()
-            
-        elif self.__face_choisie == 6:
-            # Affiche le de sur la face 6
-            de_face6 = pygame.image.load("./assets/img/de/Face6.png")
-            surface.blit(de_face6,(350,475))
-            # Mettre à jour l'affichage
-            pygame.display.update()
-            
-        return self.__face_choisie
 
--- classe/personnage/intelA.py
+++ classe/personnage/intelA.py
@@ -1,7 +1,105 @@
+import pygame
+from random import randint
 from classe.personnage import joueur
 
 class IntelA (joueur.Joueur):
     def __init__(self, id, prenom, element, plateaux, plateauy, pv, attaque, inventaire) -> None:
         super().__init__(id, prenom, element, plateaux, plateauy, pv, attaque, inventaire)
     
-    
+    def choix_perso(self, interface):
+        pass
+
+    def lancement_de(self, interface) -> int:
+        de_cliquer = False
+        while de_cliquer == False:
+            # Affiche le de sur la face 2
+            de_face2 = pygame.image.load("./assets/img/de/Face2.png")
+            interface.get_fenetre().blit(de_face2,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+            pygame.time.delay(150)
+            
+            # Affiche le de sur la face 3
+            de_face3 = pygame.image.load("./assets/img/de/Face3.png")
+            interface.get_fenetre().blit(de_face3,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+            pygame.time.delay(200)
+            
+            # Affiche le de sur la face 4
+            de_face4 = pygame.image.load("./assets/img/de/Face4.png")
+            interface.get_fenetre().blit(de_face4,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+            pygame.time.delay(250)
+            
+            # Affiche le de sur la face 5
+            de_face5 = pygame.image.load("./assets/img/de/Face5.png")
+            interface.get_fenetre().blit(de_face5,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+            pygame.time.delay(300)
+            
+            # Affiche le de sur la face 1
+            de_face1 = pygame.image.load("./assets/img/de/Face1.png")
+            interface.get_fenetre().blit(de_face1,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+            pygame.time.delay(400)
+            
+            # Affiche le de sur la face 6
+            de_face6 = pygame.image.load("./assets/img/de/Face6.png")
+            interface.get_fenetre().blit(de_face6,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+            pygame.time.delay(450)
+            de_cliquer = True
+            
+        # Choisir la face aleatoire
+        interface.get_de_jeu().set_face_choisie(randint(1,6))
+
+        if interface.get_de_jeu().get_face_choisie() == 1:
+            # Affiche le de sur la face 1
+            de_face1 = pygame.image.load("./assets/img/de/Face1.png")
+            interface.get_fenetre().blit(de_face1,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+
+        elif interface.get_de_jeu().get_face_choisie() == 2:
+            # Affiche le de sur la face 2
+            de_face2 = pygame.image.load("./assets/img/de/Face2.png")
+            interface.get_fenetre().blit(de_face2,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+
+        elif interface.get_de_jeu().get_face_choisie() == 3:
+            # Affiche le de sur la face 3
+            de_face3 = pygame.image.load("./assets/img/de/Face3.png")
+            interface.get_fenetre().blit(de_face3,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+
+        elif interface.get_de_jeu().get_face_choisie() == 4:
+            # Affiche le de sur la face 4
+            de_face4 = pygame.image.load("./assets/img/de/Face4.png")
+            interface.get_fenetre().blit(de_face4,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+
+        elif interface.get_de_jeu().get_face_choisie() == 5:
+            # Affiche le de sur la face 5
+            de_face5 = pygame.image.load("./assets/img/de/Face5.png")
+            interface.get_fenetre().blit(de_face5,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+
+        elif interface.get_de_jeu().get_face_choisie() == 6:
+            # Affiche le de sur la face 6
+            de_face6 = pygame.image.load("./assets/img/de/Face6.png")
+            interface.get_fenetre().blit(de_face6,(350,475))
+            # Mettre à jour l'affichage
+            pygame.display.update()
+
+        return interface.get_de_jeu().get_face_choisie()
+
+
